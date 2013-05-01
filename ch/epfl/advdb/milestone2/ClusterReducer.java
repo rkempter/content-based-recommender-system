1d4f1b249fa4d7fb9f86972b1bd3bca3c212ab4c
rkempter <renato.kempter@gmail.com>
2013-05-01 23:29:26 +0200

    Add setup function to get number of dimensions in vector space

--- ch/epfl/advdb/milestone2/ClusterReducer.java
+++ ch/epfl/advdb/milestone2/ClusterReducer.java
@@ -2,8 +2,11 @@ package ch.epfl.advdb.milestone2;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 
+import org.apache.ftpserver.ftplet.Configuration;
+import org.apache.ftpserver.ftplet.FtpException;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.mapred.JobConf;
@@ -13,25 +16,32 @@ import org.apache.hadoop.mapreduce.filecache.DistributedCache;
 public class ClusterReducer extends 
 	Reducer<IntWritable, MovieWritable, CentroidWritable, ClusterMoviesWritable>{
 	
+	int featureDimensions;
+	
+	public void setup(Context context) {
+		Configuration conf = (Configuration) context.getConfiguration();
+		
+		featureDimensions = conf.getInt(Constants.FEATURE_DIMENSION_STRING, Constants.NUM_OF_FEATURES);
+	}
+	
 	public void reduce(IntWritable key, 
 			Iterable<MovieWritable> values, Context context) {
 		
-		// @todo: Num of features is sometimes only 10 (for V!)
-		Float[] centroid = new Float[Constants.NUM_OF_FEATURES];
-		
-		ArrayList<Integer> movieList = new ArrayList<Integer>();
+		Float[] centroid = new Float[featureDimensions];
+		HashMap<Integer, Boolean> movies = new HashMap<Integer, Boolean>();
 		
 		Iterator<MovieWritable> iterator = values.iterator();
-		int size = 0;
+		
+		MovieWritable movie;
 		
 		while(iterator.hasNext()) {
-			size++;
-			features = iterator.next().getFeatures();
-			
-			for(IntFloatPairWritable feature : features) {
-				centroid[feature.getKey()] += feature.getValue();
-			}
+			movie = iterator.next();
+			movies.put(movie.getMovie(), true);
+			int featureNumber = movie.getFeatureNumber();
+			centroid[featureNumber] += movie.getFeatureValue();
 		}
+		
+		int size = movies.size();
 		
 		for(int feature = 0; feature < centroid.length; feature++) {
 			centroid[feature] = centroid[feature] / size;
