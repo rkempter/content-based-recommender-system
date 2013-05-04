e52bad0e8f5466617e7d4e9e040ed931a8939bae
rkempter <renato.kempter@gmail.com>
2013-05-04 21:32:08 +0200

    Test of the reservoir sampling algorithm

--- ch/epfl/advdb/milestone2/Main.java
+++ ch/epfl/advdb/milestone2/Main.java
@@ -1,14 +1,18 @@
 package ch.epfl.advdb.milestone2;
 
+import java.io.IOException;
+
 public class Main {
 
 	/*
 	 * Main function that controls the complete programm flow
 	 */
-	public static void main(String args[])
+	public static void main(String args[]) throws IOException
 	{
 		
-		// Get centroids and number of cluster
+		// Test of reservoir sampling of centroids
+		DataClustering dc = new DataClustering(args[0], args[1], 2, 1, 1, 20, 20);
+		dc.initialIMDBCentroids();
 		
 		// Cluster V as well as the imdb feature dataset (max of 10 times)
 		
