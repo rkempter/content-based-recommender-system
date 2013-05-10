b15aa24c6d25b282f92efae4454649f840c43d7c
rkempter <renato.kempter@gmail.com>
2013-05-10 08:30:31 +0200

    Proper ClusterReducer

--- ch/epfl/advdb/milestone2/ClusterReducer.java
+++ ch/epfl/advdb/milestone2/ClusterReducer.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -16,24 +17,24 @@ import org.apache.hadoop.mapreduce.filecache.DistributedCache;
 
 public class ClusterReducer extends 
 	Reducer<IntWritable, FeatureWritable, Text, FeatureWritable>{
-	
-	int featureDimensions;
+
 	String matrixType;
 	
 	public void setup(Context context) throws IOException {
 		Configuration conf = context.getConfiguration();
+		int clusterType = conf.getInt(Constants.MATRIX_TYPE, Constants.NETFLIX_CLUSTER);
 		
-		featureDimensions = conf.getInt(Constants.FEATURE_DIMENSION_STRING, Constants.NUM_OF_FEATURES);
-		
-		matrixType = "I";
+		if(clusterType == Constants.NETFLIX_CLUSTER) {
+			matrixType = "N";
+		} else {
+			matrixType = "I";
+		}
 	}
 	
 	public void reduce(IntWritable key, 
 			Iterable<FeatureWritable> values, Context context) throws IOException, InterruptedException {
 		
-		System.out.println("Key: "+key.get());
-		
-		float[] centroid = new float[featureDimensions];
+		HashMap<Integer, Float> centroid = new HashMap<Integer, Float>();
 		HashMap<Integer, Boolean> movies = new HashMap<Integer, Boolean>();
 		
 		Iterator<FeatureWritable> iterator = values.iterator();
@@ -43,22 +44,19 @@ public class ClusterReducer extends
 		while(iterator.hasNext()) {
 			movie = iterator.next();
 			movies.put(movie.getId(), true);
-			int featureNumber = movie.getFeatureNumber() - 1;
-			System.out.println("Feature Number: "+featureNumber);
-			System.out.println("Feature Value: "+movie.getFeatureValue());
-			System.out.println("Centroid at this point: "+centroid[featureNumber]);
-			System.out.println("Centroid lnegth = "+centroid.length);
-			centroid[featureNumber] += movie.getFeatureValue();
+			int featureNumber = movie.getFeatureNumber();
+			float featureValue = movie.getFeatureValue();
+			if(centroid.containsKey(featureNumber))
+				featureValue = centroid.get(featureNumber) + featureValue;
+			centroid.put(featureNumber,featureValue);
 		}
 		
 		int size = movies.size();
 		
-		for(int feature = 0; feature < centroid.length; feature++) {
-			centroid[feature] = centroid[feature] / size;
-			context.write(new Text(matrixType), new FeatureWritable(key.get(), feature, centroid[feature]));
+		for(Map.Entry<Integer, Float> entry : centroid.entrySet()) {
+			float value = entry.getValue() / size;
+			context.write(new Text(matrixType), new FeatureWritable(key.get(), entry.getKey(), value));
 		}
-		
-		
 	}
 	
 }
